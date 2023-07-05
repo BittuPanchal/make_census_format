c2d86e7bdba09d7bcb67c6725740d6db6f9a73d7
Bittu Panchal <115232340+BittuPanchal@users.noreply.github.com>
2023-07-05 17:16:33 +0530

    Update make_census_format.py

--- make_census_format.py
+++ make_census_format.py
@@ -5,21 +5,38 @@ import streamlit as st
 import base64
 from io import BytesIO
 import os
+from datetime import date
 
 
 def read_file(file):
     if file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        df = pd.read_excel(file)
+        df = pd.read_excel(file, header = 1)
     elif file.type == "application/vnd.ms-excel":
-        df = pd.read_excel(file)
+        df = pd.read_excel(file, header = 1)
     elif file.type == "text/csv":
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, header = 1)
     else:
         raise ValueError("Unsupported file format. Only XLSX, XLS, and CSV files are supported.")
     return df
 
 
-def main():
+
+
+
+def main():    
+    def add_agency_by_location(df):
+        if 'Location' in df.columns:
+            df['Agency'] = np.where(df['Location'] == 'California - San Diego', 'San Diego, CA',
+                                    np.where(df['Location'] == 'Virginia - Caring Angels', 'Winchester, VA',
+                                             np.where(df['Location'] == 'West Virginia', 'Martinsburg, WV',
+                                                      np.where(df['Location'] == 'Virginia - Countryside', 'Sterling, VA',
+                                                               np.where(df['Location'] == 'Virginia - Hillside Hospice', 'Hillside Hospice',
+                                                                        'Fairfield, CT')))))
+        else:
+            df['Agency'] = 'Fairfield, CT'
+        return df
+    
+
     def remove_location_column(df):
         if 'Location' in df.columns:
             df.drop('Location', axis=1, inplace=True)
@@ -29,6 +46,7 @@ def main():
     file = st.file_uploader("Upload file", type=["xlsx", "xls", "csv"])
     if file is not None:
         df = read_file(file)
+        df = add_agency_by_location(df)
         df = remove_location_column(df)
         df = df.rename(columns={'Job Type':'Department', 'Agency':'Location'})
         df['Employee Type'] = None
@@ -73,7 +91,11 @@ def main():
             
             csv_data.seek(0)
             b64_csv = base64.b64encode(csv_data.read()).decode()
-            href_csv = f'<a href="data:application/octet-stream;base64,{b64_csv}" download="{os.path.splitext(file.name)[0]}.{csv_file_extension}">Download Updated Data (CSV)</a>'
+            
+            today = date.today().strftime("%Y-%m-%d")
+            file_name = os.path.splitext(file.name)[0]
+
+            href_csv = f'<a href="data:application/octet-stream;base64,{b64_csv}" download="{today}_{file_name}.{csv_file_extension}">Download Updated Data (CSV)</a>'
             st.markdown(href_csv, unsafe_allow_html=True)
 
 
